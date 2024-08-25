f2d4e50d9e4705d3fa5f1ac93c4a11362b9e74d9
Jeffry Gonzalez <jeff@hypertheory.com>
2024-08-25 15:26:38 -0400

    Work on template

--- CritterStack/Program.cs
+++ CritterStack/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.UseCritterStackWithReasonableDefaults();
+builder.UseJasperFxStackWithReasonableDefaults();
 
 var app = builder.Build();
 
--- CritterStack/dev-environment/db/db.sql
+++ CritterStack/dev-environment/db/db.sql
@@ -1,2 +1,2 @@
-create database critterstack
+create database critterstackdb
        
